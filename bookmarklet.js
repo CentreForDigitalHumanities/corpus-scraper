f061c500f9b891d919d1981cbbb3dea18ad7fcec
Julian Gonggrijp <j.gonggrijp@uu.nl>
2017-01-10 17:52:45 +0100

    Factor out the translateHeader function

--- bookmarklet.js
+++ bookmarklet.js
@@ -86,6 +86,22 @@
 				if (!anchor || anchor.textContent !== 'Siguiente') return;
 				return anchor.getAttribute('href');
 			},
+			translateHeader: function(columnHeader) {
+				switch (columnHeader) {
+				// \xBA might be replaced by \ufffd on encoding mismatch.
+				case 'N\ufffd':
+				case 'N\xBA':
+					return 'number';
+				case 'A\xD1O':
+					return 'date';
+				case 'T\xCDTULO':
+					return 'text';
+				case 'CONCORDANCIA':
+					return 'contextLeft';
+				default:
+					return null;
+				}
+			},
 			columnPositions: function(section, titleLine) {
 				var firstMatch = section.innerHTML.split('\n')[1],
 				    columnHeads = titleLine.split(/(\s+)/),
@@ -96,24 +112,7 @@
 				    index = 0,
 				    l, i;
 				for (l = columnHeads.length, i = 0; i < l; i += 2) {
-					switch (columnHeads[i]) {
-					// \xBA might be replaced by \ufffd on encoding mismatch.
-					case 'N\ufffd':
-					case 'N\xBA':
-						currentHeader = 'number';
-						break;
-					case 'A\xD1O':
-						currentHeader = 'date';
-						break;
-					case 'T\xCDTULO':
-						currentHeader = 'text';
-						break;
-					case 'CONCORDANCIA':
-						currentHeader = 'contextLeft';
-						break;
-					default:
-						currentHeader = null;
-					}
+					currentHeader = this.translateHeader(columnHeads[i]);
 					range = [index];
 					index += columnHeads[i].length;
 					if (i + 1 < l) index += columnHeads[i + 1].length;
